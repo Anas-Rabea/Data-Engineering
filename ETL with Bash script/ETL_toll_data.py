ad087b8c904cac79b2e91d432df628308ab71cef
Anas Rabea <73164381+Anas-Rabea@users.noreply.github.com>
2023-03-15 20:41:12 +0200

    Update ETL_toll_data.py

--- ETL with Bash script/ETL_toll_data.py
+++ ETL with Bash script/ETL_toll_data.py
@@ -13,7 +13,7 @@ default_args = {
     'email_on_failure': True,
     'email_on_retry': True,
     'retries': 1,
-    'retry_delay': timedelta(minutes=1),
+    'retry_delay': timedelta(minutes=5),
 }
 
 dag = DAG(
